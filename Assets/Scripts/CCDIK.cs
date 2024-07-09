9ee88092db48ccd0d5a688022c30018b1d4239bc
Grace <gche3@illinois.edu>
2024-07-09 14:24:47 -0500

    forward kinematics working wahoo

--- Assets/Scripts/CCDIK.cs
+++ Assets/Scripts/CCDIK.cs
@@ -5,9 +5,18 @@ public class CCDIK : MonoBehaviour {
   public Transform Target;
   public CCDIKJoint[] joints;
 
+  bool m_active = false;
+
+  public bool active {
+      get => m_active;
+      set => m_active = value;
+  }
+
   void Update() {
-    for (int j = 0; j < joints.Length; j++) {
-      joints[j].Evaluate(Tooltip, Target, false);
+    if (m_active) {
+      for (int j = 0; j < joints.Length; j++) {
+        joints[j].Evaluate(Tooltip, Target, false);
+      }
     }
   }
 }
